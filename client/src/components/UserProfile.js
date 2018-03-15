a24356105536d8cdbdbf996df8516b3a6cb892ad
arielstitt <arielstitt@gmail.com>
2018-03-14 21:27:12 -0400

    picture from profile page populated

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
 import EditUser from './EditUser'
+import { Redirect } from 'react-router'
+import axios from 'axios'
+import {UserImage} from './styled-components/images'
 
 
 class UserProfile extends Component {
+    state = {
+        user: {
+          goals: []
+        },
+        redirect: false,
+      };
+
+      componentDidMount() {
+          const userId = this.props.match.params.userId
+          axios.get(`/api/users/${userId}`)
+          .then((response) =>{
+             const user = response.data
+          this.setState({ user }) 
+          })
+          
+      }
+
     render() {
         return (
             <div>
                 Hi from UserProfile
                 <a href="/users/:userId/edit">edit page</a> <br/>
                 <a href="/users/">users</a>
-
+                <UserImage src={this.state.user.imgUrl} alt=""/>
             </div>
         );
     }
